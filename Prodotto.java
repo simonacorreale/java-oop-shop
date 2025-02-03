a62aa3f2ddf04ebf7ae5165e9d478b90f580f4f6
simonacorreale <simona.correale.work@gmail.com>
2025-02-03 16:16:44 +0100

    feat:  print nome + codice

--- Prodotto.java
+++ Prodotto.java
@@ -63,10 +63,10 @@ public String getExtendedName() {
 public static void main(String[] args) {
 
     Prodotto prodottoExposed = new Prodotto ("Laptop", "Un potentissimo laptop da gaming", 1200.00, 24.0);
-        System.out.println(prodottoExposed);
+        System.out.println(prodottoExposed);//cella vuota (come esplicato a lezione)
+        System.out.println(("Nome prodotto: "+ prodottoExposed.getExtendedName()));
         System.out.println(("Prezzo base: " + prodottoExposed.getBasePrice() + "€"));
         System.out.println(("Prezzo con iva: " + prodottoExposed.getIvaPrice() +"€"));
-        
 }
 
 }
