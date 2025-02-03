4475d13fcfc3022e408fda21b7447b8dee7259c9
simonacorreale <simona.correale.work@gmail.com>
2025-02-03 16:01:44 +0100

    feat: costruttore

---
+++ Prodotto.java
@@ -0,0 +1,41 @@
+/* Esercizio di oggi Java Shop
+repo : java-oop-shop
+Nel progetto java-oop-shop, creare la classe Prodotto che gestisce i prodotti dello shop.
+Un prodotto è caratterizzato da:
+- codice (numero intero)
+- nome
+- descrizione
+- prezzo
+- iva
+Usate opportunamente costruttori, attributi ed eventuali altri metodi di “utilità” per fare in modo che:
+- alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
+- il prodotto esponga un metodo per avere il prezzo base
+- il prodotto esponga un metodo per avere il prezzo comprensivo di iva
+- il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice-nome
+Nella stessa cartella src aggiungete una classe Main con metodo main nella quale testate tutte le funzionalità della classe Prodotto. */
+
+import java.util.Random;
+
+public class Prodotto {
+
+    public int code;
+
+    public String name;
+
+    public String description;
+
+    public double price;
+
+    public double iva;
+
+ public Prodotto(String name, String description, double price, double iva) {
+
+    this.code = generateRandomCode();
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.iva = iva;
+
+ }
+    
+}
